0676e7a3c1a76af6f79d12a1c8c145cb0bcd02ff
Nicke <Nicke@Nicke-Dator>
2016-04-17 20:28:51 +0200

    c

---
+++ src/lab2/IntegerComparator.java
@@ -0,0 +1,10 @@
+package lab2;
+
+import java.util.*;
+
+public class IntegerComparator implements Comparator<Integer> {
+	
+	public int compare(Integer s1, Integer s2) {
+		return s1.compareTo(s2);
+	}
+}
--- src/lab2/Sign.java
+++ src/lab2/Sign.java
@@ -1,7 +1,7 @@
 package lab2;
 
-public class Sign implements Function<Double,Integer> {
-    public Integer apply(Double x) {
-        return x.compareTo(0.0d);
-    }
+public class Sign implements Function<Double, Integer> {
+	public Integer apply(Double x) {
+		return x.compareTo(0.0d);
+	}
 }
